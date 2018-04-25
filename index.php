20296ac6650474172032a4e2ab61603cf119b30f
peterju <peterju.tw@gmail.com>
2018-04-25 16:03:27 +0800

    首次first commit

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE HTML>
+<html lang="zh-hant-TW">
+<head>
+	<meta charset="utf-8">
+	<title>Web程式設計與實習期中考</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<link href="css/bootstrap-responsive.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+	<link href="color/default.css" rel="stylesheet">
+	<link rel="shortcut icon" href="img/favicon.ico">
+</head>
+<body>
+<!-- navbar -->
+<div class="navbar-wrapper">
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="navbar-inner">
+			<div class="container">
+				<!-- Responsive navbar -->
+				<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+				</a>
+				<h1 class="brand"><a href="index.php">Web程式設計與實習期中考</a></h1>
+				<!-- navigation -->
+				<nav class="pull-right nav-collapse collapse">
+				<ul id="menu-main" class="nav">
+					<!-- <li><a title="team" href="#about">About</a></li>
+					<li><a title="services" href="#services">Services</a></li>
+					<li><a title="works" href="#works">Works</a></li>
+					<li><a title="blog" href="#blog">Blog</a></li>
+					<li><a title="contact" href="#contact">Contact</a></li>
+					<li><a href="page.html">Page</a></li> -->
+				</ul>
+				</nav>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- section: contact -->
+<section id="contact" class="section green">
+<div class="container">
+	<h4>勤益人力銀行 求職人才履歷表</h4>
+	<p>表中“＊”請務必填寫</p>
+	<div class="blankdivider30">
+	</div>
+	<div class="row">
+		<div class="span12">
+			<div class="cform" id="contact-form">
+				<form action="" method="post" role="form" class="contactForm">
+					<div class="row">
+						<div class="span6">
+							<div class="field your-name form-group">
+								<input type="text" name="name" class="form-control" id="name" placeholder="姓名" />
+							</div>
+							<div class="field your-name form-group">
+								<input type="text" name="sex" class="form-control" id="sex" placeholder="性別" />
+							</div>
+							<div class="field your-email form-group">
+								<input type="text" class="form-control" name="email" id="email" placeholder="Your Email" />
+							</div>
+							<div class="field subject form-group">
+								<input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" />
+							</div>
+						</div>
+						<div class="span6">
+							<div class="field message form-group">
+								<textarea class="form-control" name="message" rows="5" placeholder="Message"></textarea>
+							</div>
+							<input type="submit" value="送出" class="btn btn-theme pull-left">
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+		<!-- ./span12 -->
+	</div>
+</div>
+</section>
+<footer>
+<div class="container">
+	<div class="row">
+		<div class="span6 offset3">
+			<ul class="social-networks">
+				<li><a href="#"><i class="icon-circled icon-bgdark icon-instagram icon-2x"></i></a></li>
+				<li><a href="#"><i class="icon-circled icon-bgdark icon-twitter icon-2x"></i></a></li>
+				<li><a href="#"><i class="icon-circled icon-bgdark icon-dribbble icon-2x"></i></a></li>
+				<li><a href="#"><i class="icon-circled icon-bgdark icon-pinterest icon-2x"></i></a></li>
+			</ul>
+			<p class="copyright">
+				Web程式設計與實習期中考
+			</p>
+		</div>
+	</div>
+</div>
+<!-- ./container -->
+</footer>
+<a href="#" class="scrollup"><i class="icon-angle-up icon-square icon-bgdark icon-2x"></i></a>
+<script src="js/jquery.js"></script>
+<script src="js/jquery.scrollTo.js"></script>
+<script src="js/jquery.nav.js"></script>
+<script src="js/jquery.localscroll-1.2.7-min.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/jquery.prettyPhoto.js"></script>
+<script src="js/isotope.js"></script>
+<script src="js/jquery.flexslider.js"></script>
+<script src="js/inview.js"></script>
+<script src="js/animate.js"></script>
+<script src="js/validate.js"></script>
+<script src="js/custom.js"></script>
+
+</body>
+</html>
